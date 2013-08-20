e22ae7ab8cdb8a24ec87ef020dc63390cb9c3a1f
Lichun Wei <lichun.william@gmail.com>
2013-08-20 20:28:40 +0800

    added a full screen view demo

--- gen/com/williamjoy/wall/english/R.java
+++ gen/com/williamjoy/wall/english/R.java
@@ -9,51 +9,72 @@ package com.williamjoy.wall.english;
 
 public final class R {
     public static final class array {
-        public static final int pref_example_list_titles=0x7f060000;
-        public static final int pref_example_list_values=0x7f060001;
-        public static final int pref_sync_frequency_titles=0x7f060002;
-        public static final int pref_sync_frequency_values=0x7f060003;
+        public static final int pref_example_list_titles=0x7f070000;
+        public static final int pref_example_list_values=0x7f070001;
+        public static final int pref_sync_frequency_titles=0x7f070002;
+        public static final int pref_sync_frequency_values=0x7f070003;
     }
     public static final class attr {
+        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+         */
+        public static final int buttonBarButtonStyle=0x7f010001;
+        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+         */
+        public static final int buttonBarStyle=0x7f010000;
+    }
+    public static final class color {
+        public static final int black_overlay=0x7f050000;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f090000;
+        public static final int dummy_button=0x7f0a0002;
+        public static final int fullscreen_content=0x7f0a0000;
+        public static final int fullscreen_content_controls=0x7f0a0001;
+        public static final int menu_settings=0x7f0a0004;
+        public static final int textViewHello=0x7f0a0003;
     }
     public static final class layout {
-        public static final int activity_learn_sentence=0x7f030000;
+        public static final int activity_fullscreen=0x7f030000;
+        public static final int activity_learn_sentence=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_learn_sentence=0x7f080000;
+        public static final int activity_learn_sentence=0x7f090000;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int menu_settings=0x7f050002;
-        public static final int pref_default_display_name=0x7f050008;
-        public static final int pref_description_social_recommendations=0x7f050006;
+        public static final int app_name=0x7f060000;
+        public static final int dummy_button=0x7f060005;
+        public static final int dummy_content=0x7f060006;
+        public static final int hello_world=0x7f060001;
+        public static final int menu_settings=0x7f060002;
+        public static final int pref_default_display_name=0x7f06000d;
+        public static final int pref_description_social_recommendations=0x7f06000b;
         /**  Example settings for Data & Sync 
          */
-        public static final int pref_header_data_sync=0x7f05000a;
+        public static final int pref_header_data_sync=0x7f06000f;
         /**  Strings related to Settings 
  Example General settings 
          */
-        public static final int pref_header_general=0x7f050004;
+        public static final int pref_header_general=0x7f060009;
         /**  Example settings for Notifications 
          */
-        public static final int pref_header_notifications=0x7f05000d;
-        public static final int pref_ringtone_silent=0x7f050010;
-        public static final int pref_title_add_friends_to_messages=0x7f050009;
-        public static final int pref_title_display_name=0x7f050007;
-        public static final int pref_title_new_message_notifications=0x7f05000e;
-        public static final int pref_title_ringtone=0x7f05000f;
-        public static final int pref_title_social_recommendations=0x7f050005;
-        public static final int pref_title_sync_frequency=0x7f05000b;
-        public static final int pref_title_system_sync_settings=0x7f05000c;
-        public static final int pref_title_vibrate=0x7f050011;
-        public static final int title_activity_learn_sentence=0x7f050003;
+        public static final int pref_header_notifications=0x7f060012;
+        public static final int pref_ringtone_silent=0x7f060015;
+        public static final int pref_title_add_friends_to_messages=0x7f06000e;
+        public static final int pref_title_display_name=0x7f06000c;
+        public static final int pref_title_new_message_notifications=0x7f060013;
+        public static final int pref_title_ringtone=0x7f060014;
+        public static final int pref_title_social_recommendations=0x7f06000a;
+        public static final int pref_title_sync_frequency=0x7f060010;
+        public static final int pref_title_system_sync_settings=0x7f060011;
+        public static final int pref_title_vibrate=0x7f060016;
+        public static final int title_activity_fullscreen=0x7f060004;
+        public static final int title_activity_learn_sentence=0x7f060003;
+        public static final int title_item_detail=0x7f060008;
+        public static final int title_item_list=0x7f060007;
     }
     public static final class style {
         /** 
@@ -77,11 +98,15 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
+        public static final int ButtonBar=0x7f080003;
+        public static final int ButtonBarButton=0x7f080004;
+        public static final int FullscreenActionBarStyle=0x7f080005;
+        public static final int FullscreenTheme=0x7f080002;
     }
     public static final class xml {
         public static final int pref_data_sync=0x7f040000;
@@ -89,4 +114,46 @@ public final class R {
         public static final int pref_headers=0x7f040002;
         public static final int pref_notification=0x7f040003;
     }
+    public static final class styleable {
+        /** 
+         Declare custom theme attributes that allow changing which styles are
+         used for button bars depending on the API level.
+         ?android:attr/buttonBarStyle is new as of API 11 so this is
+         necessary to support previous API levels.
+    
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #ButtonBarContainerTheme_buttonBarButtonStyle com.williamjoy.wall.english:buttonBarButtonStyle}</code></td><td></td></tr>
+           <tr><td><code>{@link #ButtonBarContainerTheme_buttonBarStyle com.williamjoy.wall.english:buttonBarStyle}</code></td><td></td></tr>
+           </table>
+           @see #ButtonBarContainerTheme_buttonBarButtonStyle
+           @see #ButtonBarContainerTheme_buttonBarStyle
+         */
+        public static final int[] ButtonBarContainerTheme = {
+            0x7f010000, 0x7f010001
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.williamjoy.wall.english.R.attr#buttonBarButtonStyle}
+          attribute's value can be found in the {@link #ButtonBarContainerTheme} array.
+
+
+          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+          @attr name android:buttonBarButtonStyle
+        */
+        public static final int ButtonBarContainerTheme_buttonBarButtonStyle = 1;
+        /**
+          <p>This symbol is the offset where the {@link com.williamjoy.wall.english.R.attr#buttonBarStyle}
+          attribute's value can be found in the {@link #ButtonBarContainerTheme} array.
+
+
+          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+          @attr name android:buttonBarStyle
+        */
+        public static final int ButtonBarContainerTheme_buttonBarStyle = 0;
+    };
 }
--- src/com/williamjoy/wall/english/LearnSentenceActivity.java
+++ src/com/williamjoy/wall/english/LearnSentenceActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class LearnSentenceActivity extends Activity {
 
@@ -12,11 +13,12 @@ public class LearnSentenceActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn_sentence);
+        TextView tv = (TextView) this.findViewById(R.id.textViewHello);
+        tv.setText("Love Mary!");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_learn_sentence, menu);
         return true;
     }
