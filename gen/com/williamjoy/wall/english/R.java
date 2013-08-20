0d5e57e669bfcdd08422669f563ea92c69d749ee
Lichun Wei <lichun.william@gmail.com>
2013-08-20 21:57:05 +0800

    a demo page

--- gen/com/williamjoy/wall/english/R.java
+++ gen/com/williamjoy/wall/english/R.java
@@ -22,7 +22,8 @@ public final class R {
     public static final class id {
         public static final int fullscreen_content=0x7f090001;
         public static final int given_content=0x7f090000;
-        public static final int menu_settings=0x7f090002;
+        public static final int menu_settings=0x7f090003;
+        public static final int wrapper_layout=0x7f090002;
     }
     public static final class layout {
         public static final int activity_learn_sentence=0x7f030000;
--- src/com/williamjoy/wall/english/LearnSentenceActivity.java
+++ src/com/williamjoy/wall/english/LearnSentenceActivity.java
@@ -1,10 +1,15 @@
 package com.williamjoy.wall.english;
 
+import com.williamjoy.wall.english.view.VocabularyInput;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class LearnSentenceActivity extends Activity {
@@ -13,6 +18,9 @@ public class LearnSentenceActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn_sentence);
+        ViewGroup l=(ViewGroup) this.findViewById(R.id.wrapper_layout);
+        l.addView(new VocabularyInput(getApplicationContext(), "Mary"));
+        l.addView(new VocabularyInput(getApplicationContext(), "Yahoo!"));
     }
 
     @Override
