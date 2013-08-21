ce0d63b1c0ddb3c2b028de62a7db79773b6134d9
Lichun Wei <lichun.william@gmail.com>
2013-08-21 22:16:21 +0800

    set sinle line input; use regex to cut sentences into words; recognize punctuations;

--- src/com/williamjoy/wall/english/view/VocabularyInput.java
+++ src/com/williamjoy/wall/english/view/VocabularyInput.java
@@ -8,41 +8,51 @@ import android.util.Log;
 import android.widget.EditText;
 
 public class VocabularyInput extends EditText {
-    private int status = 0;
     private String targetToken;
-    private boolean locked=false;
+    private boolean editable = true;
 
     public VocabularyInput(Context context, String targetToken) {
         super(context);
-        this.targetToken = targetToken;
+        this.targetToken = targetToken.trim();
+        if (targetToken.matches("[ .,/)(%\\$@]+")) {
+            this.editable = false;
+        }
         this.setText(targetToken);
-        this.setTextColor(Color.RED);
-        this.setMinimumWidth(50);
-        this.addTextChangedListener(new TextWatcher() {
+        if (this.isEditable()) {
+            this.setSingleLine();
+            this.setTextColor(Color.RED);
+            this.setMinimumWidth(50);
+            this.addTextChangedListener(new TextWatcher() {
 
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count,
-                    int after) {
-                // TODO Auto-generated method stub
+                @Override
+                public void beforeTextChanged(CharSequence s, int start,
+                        int count, int after) {
+                    // TODO Auto-generated method stub
 
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before,
-                    int count) {
-                // TODO Auto-generated method stub
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                // TODO Auto-generated method stub
-                if (s.toString().endsWith(" ")) {
-                    Log.v("Tokens-", s.toString());
                 }
-            }
-        });
 
+                @Override
+                public void onTextChanged(CharSequence s, int start,
+                        int before, int count) {
+                    // TODO Auto-generated method stub
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    if (s.toString().endsWith(" ")) {
+                        Log.v("Tokens-", s.toString());
+                    }
+                }
+            });
+        } else {
+            this.setFocusable(false);
+            this.setTextColor(Color.MAGENTA);
+        }
+    }
+
+    private boolean isEditable() {
+        return editable;
     }
 
     public String getTargetToken() {
