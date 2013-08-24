0e17eb88d2770f72913c6090d24c9d3f0bec7137
williamjoy <lichun.william@gmail.com>
2013-08-25 02:16:59 +0800

    add empty activity

--- gen/com/williamjoy/wall/english/R.java
+++ gen/com/williamjoy/wall/english/R.java
@@ -9,10 +9,10 @@ package com.williamjoy.wall.english;
 
 public final class R {
     public static final class array {
-        public static final int pref_example_list_titles=0x7f070000;
-        public static final int pref_example_list_values=0x7f070001;
-        public static final int pref_sync_frequency_titles=0x7f070002;
-        public static final int pref_sync_frequency_values=0x7f070003;
+        public static final int pref_example_list_titles=0x7f080000;
+        public static final int pref_example_list_values=0x7f080001;
+        public static final int pref_sync_frequency_titles=0x7f080002;
+        public static final int pref_sync_frequency_values=0x7f080003;
     }
     public static final class attr {
         /** <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
@@ -83,11 +83,22 @@ containing a value of this type.
          */
         public static final int verticalSpacing=0x7f010001;
     }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+
+         Customize dimensions originally defined in res/values/dimens.xml (such as
+         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
+    
+         */
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_forgot_password=0x7f05000e;
+        public static final int action_forgot_password=0x7f05000f;
+        public static final int action_settings=0x7f050010;
         public static final int button1=0x7f050004;
         public static final int email=0x7f050008;
         public static final int given_content=0x7f050002;
@@ -96,67 +107,72 @@ containing a value of this type.
         public static final int login_form=0x7f050007;
         public static final int login_status=0x7f050005;
         public static final int login_status_message=0x7f050006;
-        public static final int menu_login=0x7f05000d;
-        public static final int menu_settings=0x7f05000c;
+        public static final int menu_login=0x7f05000e;
+        public static final int menu_settings=0x7f05000d;
         public static final int passage_layout=0x7f050003;
         public static final int password=0x7f050009;
+        public static final int scrollView1=0x7f05000c;
         public static final int sign_in_button=0x7f05000b;
         public static final int vertical=0x7f050001;
     }
     public static final class layout {
         public static final int activity_learn_sentence=0x7f030000;
         public static final int activity_login=0x7f030001;
-        public static final int edit_word=0x7f030002;
+        public static final int activity_material_editor=0x7f030002;
+        public static final int edit_word=0x7f030003;
     }
     public static final class menu {
-        public static final int activity_learn_sentence=0x7f090000;
-        public static final int login=0x7f090001;
+        public static final int activity_learn_sentence=0x7f0a0000;
+        public static final int login=0x7f0a0001;
+        public static final int material_editor=0x7f0a0002;
     }
     public static final class string {
-        public static final int action_forgot_password=0x7f06000f;
-        public static final int action_sign_in_register=0x7f06000d;
-        public static final int action_sign_in_short=0x7f06000e;
-        public static final int app_name=0x7f060000;
-        public static final int dummy_button=0x7f060006;
-        public static final int error_field_required=0x7f060014;
-        public static final int error_incorrect_password=0x7f060013;
-        public static final int error_invalid_email=0x7f060011;
-        public static final int error_invalid_password=0x7f060012;
-        public static final int hello_world=0x7f060001;
-        public static final int learn_content=0x7f060007;
-        public static final int login_progress_signing_in=0x7f060010;
-        public static final int menu_login=0x7f060003;
-        public static final int menu_settings=0x7f060002;
-        public static final int pref_default_display_name=0x7f060019;
-        public static final int pref_description_social_recommendations=0x7f060017;
+        public static final int action_forgot_password=0x7f070011;
+        public static final int action_settings=0x7f07000b;
+        public static final int action_sign_in_register=0x7f07000f;
+        public static final int action_sign_in_short=0x7f070010;
+        public static final int app_name=0x7f070000;
+        public static final int dummy_button=0x7f070006;
+        public static final int error_field_required=0x7f070016;
+        public static final int error_incorrect_password=0x7f070015;
+        public static final int error_invalid_email=0x7f070013;
+        public static final int error_invalid_password=0x7f070014;
+        public static final int hello_world=0x7f070001;
+        public static final int learn_content=0x7f070007;
+        public static final int login_progress_signing_in=0x7f070012;
+        public static final int menu_login=0x7f070003;
+        public static final int menu_settings=0x7f070002;
+        public static final int pref_default_display_name=0x7f07001b;
+        public static final int pref_description_social_recommendations=0x7f070019;
         /**  Example settings for Data & Sync 
          */
-        public static final int pref_header_data_sync=0x7f06001b;
+        public static final int pref_header_data_sync=0x7f07001d;
         /**  Strings related to Settings 
  Example General settings 
          */
-        public static final int pref_header_general=0x7f060015;
+        public static final int pref_header_general=0x7f070017;
         /**  Example settings for Notifications 
          */
-        public static final int pref_header_notifications=0x7f06001e;
-        public static final int pref_ringtone_silent=0x7f060021;
-        public static final int pref_title_add_friends_to_messages=0x7f06001a;
-        public static final int pref_title_display_name=0x7f060018;
-        public static final int pref_title_new_message_notifications=0x7f06001f;
-        public static final int pref_title_ringtone=0x7f060020;
-        public static final int pref_title_social_recommendations=0x7f060016;
-        public static final int pref_title_sync_frequency=0x7f06001c;
-        public static final int pref_title_system_sync_settings=0x7f06001d;
-        public static final int pref_title_vibrate=0x7f060022;
+        public static final int pref_header_notifications=0x7f070020;
+        public static final int pref_ringtone_silent=0x7f070023;
+        public static final int pref_title_add_friends_to_messages=0x7f07001c;
+        public static final int pref_title_display_name=0x7f07001a;
+        public static final int pref_title_new_message_notifications=0x7f070021;
+        public static final int pref_title_ringtone=0x7f070022;
+        public static final int pref_title_social_recommendations=0x7f070018;
+        public static final int pref_title_sync_frequency=0x7f07001e;
+        public static final int pref_title_system_sync_settings=0x7f07001f;
+        public static final int pref_title_vibrate=0x7f070024;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f06000b;
-        public static final int prompt_password=0x7f06000c;
-        public static final int title_activity_fullscreen=0x7f060005;
-        public static final int title_activity_learn_sentence=0x7f060004;
-        public static final int title_activity_login=0x7f06000a;
-        public static final int title_item_detail=0x7f060009;
-        public static final int title_item_list=0x7f060008;
+        public static final int prompt_email=0x7f07000d;
+        public static final int prompt_password=0x7f07000e;
+        public static final int title_activity_fullscreen=0x7f070005;
+        public static final int title_activity_learn_sentence=0x7f070004;
+        public static final int title_activity_login=0x7f07000c;
+        public static final int title_activity_material_editor=0x7f07000a;
+        public static final int title_item_detail=0x7f070009;
+        public static final int title_item_list=0x7f070008;
     }
     public static final class style {
         /** 
@@ -180,14 +196,14 @@ containing a value of this type.
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f080000;
+        public static final int AppBaseTheme=0x7f090000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f080001;
-        public static final int ButtonBar=0x7f080002;
-        public static final int ButtonBarButton=0x7f080003;
-        public static final int LoginFormContainer=0x7f080004;
+        public static final int AppTheme=0x7f090001;
+        public static final int ButtonBar=0x7f090002;
+        public static final int ButtonBarButton=0x7f090003;
+        public static final int LoginFormContainer=0x7f090004;
     }
     public static final class xml {
         public static final int pref_data_sync=0x7f040000;
---
+++ src/com/williamjoy/wall/english/MaterialEditorActivity.java
@@ -0,0 +1,22 @@
+package com.williamjoy.wall.english;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MaterialEditorActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material_editor);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.material_editor, menu);
+        return true;
+    }
+
+}
