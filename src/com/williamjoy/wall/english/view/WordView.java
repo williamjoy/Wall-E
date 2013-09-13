99c933d1d2915e62dde3377d813d8fdb3b738c11
williamjoy <lichun.william@gmail.com>
2013-09-13 21:30:40 +0800

    WordView.java

---
+++ src/com/williamjoy/wall/english/view/WordView.java
@@ -0,0 +1,25 @@
+package com.williamjoy.wall.english.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+
+public class WordView extends TextView {
+
+	public WordView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public WordView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public WordView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+}
